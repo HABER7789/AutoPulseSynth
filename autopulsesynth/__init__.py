3662b7e615167c986497b14f5949bdc4de83d52c
dhairyapatel514 <patel.dhairya@northeastern.edu>
2026-02-12 04:50:36 -0500

    Expose export functionality and update notebook

--- autopulsesynth/__init__.py
+++ autopulsesynth/__init__.py
@@ -9,10 +9,14 @@ All results are simulated.
 
 from .model import QubitHamiltonianModel, UncertaintyModel
 from .pulses import GaussianDragPulse, clip_and_smooth
-from .simulate import simulate_evolution, fidelity_metric
-from .optimize import SurrogateDataset, train_surrogate, optimize_under_uncertainty
+from .simulate import simulate_evolution, fidelity_metric, simulate_unitary, target_unitary
+from .optimize import SurrogateDataset, train_surrogate, optimize_under_uncertainty, verify_in_simulator
 from .utils import hz_to_rad_s, rad_s_to_hz
 from .metrics import average_gate_fidelity_unitary, average_state_fidelity_proxy
+from .export import export_pulse_json
+
+# Backwards compatibility aliases for notebook
+average_gate_fidelity = average_gate_fidelity_unitary
 
 __all__ = [
     "QubitHamiltonianModel",
@@ -20,10 +24,15 @@ __all__ = [
     "GaussianDragPulse",
     "clip_and_smooth",
     "simulate_evolution",
+    "simulate_unitary",
     "fidelity_metric",
+    "target_unitary",
     "SurrogateDataset",
     "train_surrogate",
     "optimize_under_uncertainty",
+    "verify_in_simulator",
     "hz_to_rad_s",
     "average_gate_fidelity_unitary",
+    "average_gate_fidelity",
+    "export_pulse_json",
 ]
